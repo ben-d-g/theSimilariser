feeb3f3a104a0f0a65b99386caae8992dd24fb7c
Ben <ben.d.godfrey2002@gmail.com>
2024-06-20 00:34:03 +0100

    removed unneccessary code in __init__

--- source/theSimilariser.py
+++ source/theSimilariser.py
@@ -2,14 +2,7 @@ import requests
 
 class Similariser:
     def __init__(self, term1 = "", term2 = ""):
-        self.term1 = term1
-        self.term2 = term2
-
-        self.url1 = "https://en.wikipedia.org/wiki/" + self.spaceToUnderscore(term1)
-        self.url2 = "https://en.wikipedia.org/wiki/" + self.spaceToUnderscore(term2)
-
-        self.linkSet1 = set(self.pagesLinkedFrom(self.url1))
-        self.linkSet2 = set(self.pagesLinkedFrom(self.url2))
+        self.updateTerms(term1, term2)
 
     def updateTerms(self, term1, term2):
         self.term1 = term1
